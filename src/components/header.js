607b48e6bbde3d8326ae2d7c8b22e0e194929fc3
conradoRC <conrado27cym@gmail.com>
2019-12-31 04:00:32 -0600

    Finish-refactor, proyect container

--- src/components/header.js
+++ src/components/header.js
@@ -3,18 +3,19 @@ import illustration from '../images/undraw_absorbed.svg';
 
 export default () => (
     <header className= "bg-blue-700">
-        <div className= "container mx-auto p-12 max-w-4xl">
-            <div className= "flex justify-center items-center">
-                <div className="flex-1">
+        <div className= "container mx-auto p-4 max-w-4xl text-center">
+                <div className="flex-1 ">
                     <h1 className="font-bold text-white text-5xl">
                         ¡Hola, yo soy Conrado!                    
                     </h1>
-                    <p className= "text-xl text-blue-200">Desarrollo aplicaciones web</p>
+                    <p className= "text-xl text-blue-100 my-1 mb-2 ">Desarrollo aplicaciones web</p>
                 </div>
+            <div className= "flex justify-center items-center">
                 <img src={illustration} alt ="Persona leyendo mientras camina" style={{height: "300px"}}></img>
             </div>
             <div>
-            <label htmlFor="contact-content" className="block text-gray-800 text-sm font-bold">
+            <label htmlFor="contact-content" 
+                   className="block text-blue-100 text-sm py-4">
                 Aprendiendo día con día de las mejores tecnologías, unicamente pensando en ti.
             </label>
         </div>
--- src/components/repos.js
+++ src/components/repos.js
@@ -1,10 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Repo from './repo';
 import icongit from '../images/icons/icons-git.svg';
+import repos from '../data/datas';
+
 
 export default () => {
     
-    const [repos,setRepos]= useState([]);
+    return  (
+    <div className="xl:px-40 lg:px-40 md:px-16 sm:px-16 px-6">
+        <div className="box-shadow rounded-lg my-8 pb-4">
+             <img src={icongit} alt ="icono de git"
+                style={{height: "64px" ,float:"right"}}>
+             </img>
+             <header className="text-center">
+                <h2 className="text-3xl font-bold pt-4 ml-16">
+                    Mi trabajo en Open Source
+                </h2>   
+                <p className="my-3">
+                    Colaboración y contribución de código  
+                </p>
+            </header>
+            <div className="flex flex-wrap min-h-full ">
+                { repos.map((repo) => {
+                    return <Repo repo= {repo} key = {repo.id}/>
+                })}
+            </div>
+            <div className="flex justify-end text-center mt-6 mb-2 mr-5">
+                    <a className="text-blue-700 tarjet-shadow font-bold pb-2 pt-1 w-32 rounded text-sm hover:bg-blue-600 hover:text-white" 
+                        href="https://github.com/conradRC" 
+                        tab="_blank" rel="noopener noreferrer">
+                            Ver mas ({6})
+                    </a>
+            </div>
+        </div>
+    </div>  
+    );
+};
+
+/*    const [repos,setRepos]= useState([]);
     const [reposCount, setReposCount] = useState([]);
 
     useEffect( () => {
@@ -13,7 +46,7 @@ export default () => {
         if(data){
             myRepos = JSON.parse(data);
             setReposCount(myRepos.length);
-            myRepos = myRepos.slice(1,7);
+            myRepos = myRepos.slice(1,9);
             setRepos(myRepos);
         }
 
@@ -29,36 +62,4 @@ export default () => {
         
     },[]);
 
-
-    return  (
-    <div className="xl:px-40 lg:px-40 md:px-20 sm:px-24 px-12">
-        <div className="shadow-box my-8 pb-4">
-             <img src={icongit} alt ="icono de git"
-                style={{height: "64px" ,float:"right"}}>
-             </img>
-             <header className="text-center">
-                <h2 className="text-3xl font-bold pt-4 ml-16">
-                    Mi trabajo en Open Source
-                </h2>   
-                <p className="my-3">
-                    Colaboración y contribución de código  
-                </p>
-            </header>
-            <div className="flex flex-wrap h-full ">
-                { repos.map((repo) => {
-                    return <Repo repo= {repo} key = {repo.id}/>
-                })}
-                
-            </div>
-            <div className="flex justify-end text-center mt-6 mb-2 mr-5">
-                    <a className="text-white font-bold  bg-blue-600 pb-2 pt-1 shadow w-32 rounded text-sm" 
-                        href="https://github.com/conradRC" 
-                        tab="_blank" rel="noopener noreferrer">
-                            Ver mas ({reposCount})
-                    </a>
-            </div>
-        </div>
-    </div>  
-    );
-};
-
+*/
