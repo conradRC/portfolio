54b6d4affd342238ed7ef6baac8adb95b76ef55a
conradoRC <conrado27cym@gmail.com>
2020-02-03 02:45:28 -0600

    finished changes!

--- src/components/Home/Banner/index.js
+++ src/components/Home/Banner/index.js
@@ -4,23 +4,18 @@ import './css/index.css';
 
 export default () => (
     <div>
-         <div className="flex justify-center">
-            <button className="button-about text-blue-600 px-8 py-4 my-10 rounded-lg font-bold bg-white text-center text-2xl hover:bg-blue-500 hover:text-white">
-               Acerca de mi
-            </button>
-        </div>
-
-        <div className="m-auto mb-6 w-6/12">
-            <p className="text-blue-700 font-serif text-center pt-6">
-                Soy Conrado y vivo en Oaxaca-México.
-                Actualmente soy estudiante en el ITO.
+        
+        <div className="m-auto w-6/12 mt-10 mb-3">
+            <p className="font text-center p-3">
+                Soy Conrado y vivo en Oaxaca-México y
+                actualmente soy estudiante en el ITO.
                 En mi tiempo libre me gusta aprender música, ver series y
                 leer distintas fuentes de información con la intención de mantenerme siempre actualizado.
             </p>
         </div>
 
         <div className="flex justify-center items-center py-6">
-            <img src={illustration} alt="Persona leyendo mientras camina" style={{ height: "250px" }}></img>
+            <img src={illustration} alt="Computadora de escritorio" style={{ height: "230px" }}></img>
         </div>
 
     </div>
--- src/components/Home/Repositorios/repos.js
+++ src/components/Home/Repositorios/repos.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import Repo from './repo';
 import icongit from '../../../images/icons/icons-git.svg';
-import repos from '../../../data/datas';
-
 export default () => {
 
+    const [repos, setRepos] = useState([]);
+    const [reposCount, setReposCount] = useState([]);
+
+    useEffect(() => {
+        const data = sessionStorage.getItem("repos");
+        let myRepos;
+        if (data) {
+            myRepos = JSON.parse(data);
+            setReposCount(myRepos.length);
+            myRepos = myRepos.slice(1, 9);
+            setRepos(myRepos);
+        }
+
+        async function fetchRepos() {
+            const response = await fetch("https://api.github.com/users/conradRC/repos");
+            myRepos = await response.json();
+            setReposCount(myRepos.length);
+            myRepos = myRepos.slice(1, 9);
+            sessionStorage.setItem("repos", JSON.stringify(myRepos));
+            setRepos(myRepos);
+        }
+        fetchRepos();
+
+    }, []);
+
+
     return (
+
+
         <div className="xl:px-40 lg:px-40 md:px-16 sm:px-16 p-6 mb-16">
             <div className="box-shadow rounded-lg my-4 pb-4 py-6">
                 <img src={icongit} alt="icono de git"
@@ -28,10 +54,9 @@ export default () => {
                     <a className="text-blue-700 tarjet-shadow font-bold mb-2 pb-2 pt-1 w-32 rounded text-sm hover:bg-blue-500 hover:text-white"
                         href="https://github.com/conradRC"
                         tab="_blank" rel="noopener noreferrer">
-                        Ver mas ({6})
+                        Ver mas ({reposCount})
                     </a>
                 </div>
-                
             </div>
             <style>
                 {`
@@ -44,29 +69,3 @@ export default () => {
     );
 };
 
-/*    const [repos,setRepos]= useState([]);
-    const [reposCount, setReposCount] = useState([]);
-
-    useEffect( () => {
-        const data = sessionStorage.getItem("repos");
-        let myRepos;
-        if(data){
-            myRepos = JSON.parse(data);
-            setReposCount(myRepos.length);
-            myRepos = myRepos.slice(1,9);
-            setRepos(myRepos);
-        }
-
-        async function fetchRepos(){
-            const response = await fetch("https://api.github.com/users/conradRC/repos");
-            myRepos = await response.json();
-            setReposCount(myRepos.length);
-            myRepos = myRepos.slice(1,7);
-            sessionStorage.setItem("repos",JSON.stringify(myRepos));
-            setRepos(myRepos);
-        }
-        fetchRepos();
-
-    },[]);
-
-*/
